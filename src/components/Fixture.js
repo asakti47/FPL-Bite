3c4b2475fb37d99ec1d0ee00f6a171ab10fb9e9e
Argy Sakti <argyaputra3@gmail.com>
2024-02-20 23:22:43 -0500

    Improved design

--- src/components/Fixture.js
+++ src/components/Fixture.js
@@ -1,34 +1,34 @@
 import React from 'react';
-import teamLogos from '../utils/teamLogos';  // Ensure the path is correct based on your project structure
-import './Fixture.css'
+import teamLogos from '../utils/teamLogos'; // Make sure the import path matches your project structure
+import './Fixture.css';
 
 const Fixture = ({ homeTeam, awayTeam, homeStats, awayStats }) => {
     return (
         <div className="fixture">
-            <div className="fixture-teams">
-                <div className="team home-team">
-                    <img src={teamLogos[homeTeam]} alt={`${homeTeam} logo`} className="team-logo" />
-                    <span>{homeTeam}</span>
-                </div>
-                <span className="vs">vs</span>
-                <div className="team away-team">
-                    <span>{awayTeam}</span>
-                    <img src={teamLogos[awayTeam]} alt={`${awayTeam} logo`} className="team-logo" />
-                </div>
+            {/* Home Team Stats */}
+            <div className="team-stats home-stats">
+                <div className="stat-box">xG: {homeStats.xG}</div>
+                <div className="stat-box">xGA: {homeStats.xGA}</div>
             </div>
-            <div className="fixture-stats">
-                <div className="team-stats home-stats">
-                    <div className="stat-box">xG: {homeStats.xG}</div>
-                    <div className="stat-box">xGA: {homeStats.xGA}</div>
-                </div>
-                <div className="team-stats away-stats">
-                    <div className="stat-box">xG: {awayStats.xG}</div>
-                    <div className="stat-box">xGA: {awayStats.xGA}</div>
-                </div>
+            
+            {/* Home Team Name and Logo */}
+            <span className="team-name">{homeTeam}</span>
+            <img src={teamLogos[homeTeam]} alt={`${homeTeam} logo`} className="team-logo" />
+            
+            {/* VS Section */}
+            <span className="vs">vs</span>
+            
+            {/* Away Team Logo and Name */}
+            <img src={teamLogos[awayTeam]} alt={`${awayTeam} logo`} className="team-logo" />
+            <span className="team-name">{awayTeam}</span>
+            
+            {/* Away Team Stats */}
+            <div className="team-stats away-stats">
+                <div className="stat-box">xG: {awayStats.xG}</div>
+                <div className="stat-box">xGA: {awayStats.xGA}</div>
             </div>
         </div>
     );
 };
 
 export default Fixture;
-
